5520ef442bd07c45d6b09e08797b6e95280484ae
Marco Zellini <zellinimarco@gmail.com>
2026-03-26 21:40:39 +0100

    fix: linting

--- app/app.vue
+++ app/app.vue
@@ -32,29 +32,31 @@ const items = computed<NavigationMenuItem[]>(() => [
     active: route.path.startsWith('/courses')
   }
 ])
-
 </script>
 
 <template>
   <UApp>
     <UHeader>
       <template #left>
-        <NuxtLink to="/" class="flex items-center gap-2">
-          <img src="/logo.png" alt="Rent Ride Pesaro Logo" class="w-auto h-12 shrink-0" />
+        <NuxtLink
+          to="/"
+          class="flex items-center gap-2"
+        >
+          <img
+            src="/logo.png"
+            alt="Rent Ride Pesaro Logo"
+            class="w-auto h-12 shrink-0"
+          >
           <div class="text-xl font-bold">Rent&Ride</div>
         </NuxtLink>
       </template>
-
-
 
       <template #right>
         <UNavigationMenu :items="items" />
       </template>
     </UHeader>
 
-    <UMain>
-
-    </UMain>
+    <UMain />
 
     <UFooter>
       <template #left>
@@ -65,12 +67,24 @@ const items = computed<NavigationMenuItem[]>(() => [
 
       <template #right>
         <UTooltip text="Instagram">
-          <UButton to="https://www.instagram.com/rent_ride_pesaro/" target="_blank" icon="i-simple-icons-instagram"
-            aria-label="Instagram" color="neutral" variant="ghost" />
+          <UButton
+            to="https://www.instagram.com/rent_ride_pesaro/"
+            target="_blank"
+            icon="i-simple-icons-instagram"
+            aria-label="Instagram"
+            color="neutral"
+            variant="ghost"
+          />
         </UTooltip>
         <UTooltip text="Facebook">
-          <UButton to="https://www.facebook.com/p/RentRide-100083296144373/" target="_blank"
-            icon="i-simple-icons-facebook" aria-label="Facebook" color="neutral" variant="ghost" />
+          <UButton
+            to="https://www.facebook.com/p/RentRide-100083296144373/"
+            target="_blank"
+            icon="i-simple-icons-facebook"
+            aria-label="Facebook"
+            color="neutral"
+            variant="ghost"
+          />
         </UTooltip>
       </template>
     </UFooter>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,8 +4,8 @@ export default <Partial<Config>>{
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Comic Neue', 'sans-serif'],
-      },
-    },
-  },
+        sans: ['Comic Neue', 'sans-serif']
+      }
+    }
+  }
 } satisfies Config
